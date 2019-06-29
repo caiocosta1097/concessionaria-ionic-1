5caabaad9b04f7b0a68f7605f97ae7ef76de5c62
Caio Costa <caio.costacarmo@gmail.com>
2019-06-29 18:15:44 -0300

    Detalhes do carro

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,15 +1,37 @@
 angular.module('starter').controller('listaController', function ($scope) {
 
     $scope.listaCarros = [
-        'Golf 2.0',
-        'Celta 1.4',
-        'Azera 2.0',
-        'Hilux 3.0',
-        'Fusion 2.4',
-        'Onix 1.6',
-        'Sandeiro 1.8',
-        'Civic 2.0',
-        'Corolla 1.8'
+        {"modelo": "Golf 2.0", "preco": 100000},
+        {"modelo": "Celta 1.4", "preco": 25000},
+        {"modelo": "Azera 2.0", "preco": 120000},
+        {"modelo": "Hilux 3.0", "preco": 135000},
+        {"modelo": "Fusion 2.4", "preco": 145000},
+        {"modelo": "Onix 1.6", "preco": 4500},
+        {"modelo": "Sandeiro 1.8", "preco": 38000},
+        {"modelo": "Civic 2.0", "preco": 99000},
+        {"modelo": "Corolla 1.8", "preco": 105000}
     ];
 
-});
+})
+
+angular.module('starter').controller('detalhesController', function ($stateParams, $scope) {
+
+    $scope.carro = angular.fromJson($stateParams.carro);
+
+    $scope.listaAcessorios = [
+        {"nome": "Freio ABS", "preco": 800},
+        {"nome": "Ar Condicionado", "preco": 1000},
+        {"nome": "MP3 Player", "preco": 500}
+    ];
+
+    $scope.click = (acessorio, IsChecked) => {
+
+        if(IsChecked){
+            $scope.carro.preco += acessorio.preco;
+        }else{
+            $scope.carro.preco -= acessorio.preco;
+        }
+
+    }
+
+})
--- www/js/routes.js
+++ www/js/routes.js
@@ -6,6 +6,12 @@ $stateProvider.state('listagem', {
     url: '/listagem',
     templateUrl: 'templates/listagem.html',
     controller: 'listaController'
-});
+})
+
+$stateProvider.state('detalhes', {
+    url: '/detalhes/:carro',
+    templateUrl: 'templates/carroDetalhes.html',
+    controller: 'detalhesController'
+})
 
 });
