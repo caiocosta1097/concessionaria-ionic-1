8f28ca1100e98606505d9018b4d586546801108f
Caio Costa <caio.costacarmo@gmail.com>
2019-06-29 13:20:31 -0300

    Exibindo lista através da view

---
+++ www/js/controller.js
@@ -0,0 +1,15 @@
+angular.module('starter').controller('listaController', function ($scope) {
+
+    $scope.listaCarros = [
+        'Golf 2.0',
+        'Celta 1.4',
+        'Azera 2.0',
+        'Hilux 3.0',
+        'Fusion 2.4',
+        'Onix 1.6',
+        'Sandeiro 1.8',
+        'Civic 2.0',
+        'Corolla 1.8'
+    ];
+
+});
---
+++ www/js/routes.js
@@ -0,0 +1,11 @@
+angular.module('starter').config(function($stateProvider, $urlRouterProvider){
+
+$urlRouterProvider.otherwise('listagem');
+
+$stateProvider.state('listagem', {
+    url: '/listagem',
+    templateUrl: 'templates/listagem.html',
+    controller: 'listaController'
+});
+
+});
