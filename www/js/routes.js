9363f0954a89d43c2f1c0536adf755e86a28813d
Caio Costa <caio.costacarmo@gmail.com>
2019-07-02 18:05:19 -0300

    Tela de login

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,20 +1,26 @@
 angular.module('starter').config(function($stateProvider, $urlRouterProvider){
 
-$urlRouterProvider.otherwise('listagem');
+$urlRouterProvider.otherwise('login');
 
-$stateProvider.state('listagem', {
+$stateProvider
+.state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html'
+})
+
+.state('listagem', {
     url: '/listagem',
     templateUrl: 'templates/listagem.html',
     controller: 'listaController'
 })
 
-$stateProvider.state('detalhes', {
+.state('detalhes', {
     url: '/detalhes/:carro',
     templateUrl: 'templates/carroDetalhes.html',
     controller: 'detalhesController'
 })
 
-$stateProvider.state('finalizar', {
+.state('finalizar', {
     url: '/finalizar/:carro',
     templateUrl: 'templates/finalizarPedido.html',
     controller: 'finalizarController'
